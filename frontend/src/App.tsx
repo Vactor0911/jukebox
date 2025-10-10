c83b5a37c2bd92e0d9afdb934c4e30f8b1d83739
Vactor0911 <vactor0911@gmail.com>
2025-10-10 15:16:19 +0900

    헤더 컴포넌트 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { Home } from "./pages";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./utils/theme";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter basename="jukebox">
         <CssBaseline />
+        <Header />
+        
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
